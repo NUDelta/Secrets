d820876aeacc985261301f3425d66665fbc627cc
cegrief <cegrief@gmail.com>
2014-05-02 02:29:35 -0500

    Updated Submission form and first iteration of file upload
    File upload is not quite working however, imgur API is being tricky.

--- includes/secretsubmit.js
+++ includes/secretsubmit.js
@@ -1,22 +1,28 @@
-Parse.initialize("fp7oxuptKJ9ysesuXOeV4Ieul8ErSZklVwRslkJW", "HLpukqho21z1LaL7dUrPMRWI0jAu38NqmmL9qIfo");
-
-function submit(){
-	var values = {};
-	$.each($('#myForm').serializeArray(), function(i, field) {
-    values[field.name] = field.value;
+$(document).ready(function(){
+	Parse.initialize("fp7oxuptKJ9ysesuXOeV4Ieul8ErSZklVwRslkJW", "HLpukqho21z1LaL7dUrPMRWI0jAu38NqmmL9qIfo");
+	$('#picture').change(function(e){
+		var files = e.target.files;
+		upload(files[0]);
+	});
 });
 
+
+function submit(){
 	 
 	var NorthwesternSecrets = Parse.Object.extend("NorthwesternSecrets"); 
 	var secret = new NorthwesternSecrets();
 	secret.save(
 	{
-		Secret: values["secretTitle"],
-		Category: values["category"],
-		secretLocation: values["location"],
-		Directions: $('#directions').val(),
+		Secret: $('#title').val(),
+		Category: $('#category').val(),
+		secretLocation: $('#location').val(),
+		Directions: $('#secret').val(),
+		Proof: $('#proof').val(),
+		Summary: $('#summary').val(),
 		conditionForSharingWithSomeoneElse: $('#task').val(),
-		done: "no"
+		done: "no",
+		Name:"testUser",
+		Image: "secret.jpg"
 	},
 	{
 		success: function(object){
@@ -30,3 +36,37 @@ function submit(){
 		}
 	});
 }
+
+function upload(file) {
+ 
+  // file is from a <input> tag or from Drag'n Drop
+  // Is the file an image?
+ 
+  if (!file || !file.type.match(/image.*/)) return;
+ 
+  // It is!
+  // Let's build a FormData object
+ 
+  var fd = new FormData();
+  fd.append("image", file);
+   // Append the file
+/*  fd.append("key", "9c3b5dd69c6a11a80f5747566df6fb6b1ed6892b");
+  // Get your own key: http://api.imgur.com/*/
+ 
+  // Create the XHR (Cross-Domain XHR FTW!!!)
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", "http://api.imgur.com/3/image.json");
+  xhr.onload = function() {
+    // Big win!
+    // The URL of the image is:
+    console.log('success')
+    var link = JSON.parse(xhr.responseText).data.link
+      console.log(JSON.parse(xhr.responseText).data.link);
+    console.log(JSON.parse(xhr.responseText).upload.links.imgur_page);
+   }
+   // Ok, I don't handle the errors. An exercice for the reader.
+   // And now, we send the formdata
+   xhr.setRequestHeader('Authorization', 'Client-ID 25452dcdd5e816d')
+   xhr.send(fd);
+ }
