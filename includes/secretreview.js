e1df7b8a2e3b39cff6b1c7783e4c9df4d307fbfc
cegrief <cegrief@gmail.com>
2014-05-16 11:21:18 -0500

    Emails for approval and denying

--- includes/secretreview.js
+++ includes/secretreview.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 	Parse.initialize("fp7oxuptKJ9ysesuXOeV4Ieul8ErSZklVwRslkJW", "HLpukqho21z1LaL7dUrPMRWI0jAu38NqmmL9qIfo");
+	var currentUser = Parse.User.current()
+	if (currentUser) {
+    	$("#login").hide()
+    	$("#completed").show()
+    	$("#profile").html('<img src ="profile.jpg" style = "height:30px; margin-right:5px"></img>       '+ currentUser.get("username")+'<b class = "caret"></b>')
+    }
 	reviewTable();
 });
 
@@ -7,12 +13,13 @@ function reviewTable(){
 	var Submission = Parse.Object.extend("Submission");
 	var query = new Parse.Query(Submission)
 	query.include("secretID")
+	query.include("UserID")
 	query.equalTo("done", "IP")
 	query.equalTo("ownerID", Parse.User.current())
 	query.find({
 		success: function(results){
 			for(var i = 0; i< results.length; i++){
-				var data = '<tr onclick = "current(this)" class ='+ results[i].id +' ><td class = "stitle">' + results[i].get('secretID').get('Secret') +'</td><td>' + 
+				var data = '<tr onclick = "current(this)" class ="'+ results[i].id +'" myemail ="'+results[i].get("UserID").get("email")+'" username ="'+results[i].get("UserID").getUsername()+'"  ><td class = "stitle">' + results[i].get('secretID').get('Secret') +'</td><td>' + 
 				results[i].get("secretID").get('conditionForSharingWithSomeoneElse')+ '</td><td>' + 
 				results[i].get('secretID').get('submission')+'</td><td>' + 
 				results[i].get('secretID').get('lat')+' '+ results[i].get('secretID').get('long') + '</td></tr>'
@@ -27,6 +34,8 @@ function current(thisthingy){
 		+ $(thisthingy).children("td").html() +"</p><p><b>Submission: </b>" 
 		+ $(thisthingy).children("td:nth-child(3)").html() +"</p><button onclick = approve()>Approve</button><button onclick = deny()>Deny</button>");
 		currentSecretID = $(thisthingy).attr('class');
+		username = $(thisthingy).attr('username')
+		myemail = $(thisthingy).attr('myemail')
 }
 
 function approve(){
@@ -40,7 +49,15 @@ function approve(){
 			secret[0].set("new", true)
 			secret[0].save(null, {
 				success: function(){
-					location.reload();
+					var req = $.ajax( {url: "http://secrets.ci.northwestern.edu:3000",
+						type: "GET",
+						data: {name: username, 
+					       email: myemail,
+					       type:"completed" }
+				 	})
+		     		.done(function() {
+						window.location.href = "reviewSecrets.html?submit=true"
+			 		})
 				}
 			})
 		},
@@ -58,8 +75,15 @@ function deny(){
 			secret[0].set("done", "no");
 			secret[0].save(null, {
 				success: function(secret){
-					alert('Secret denied');
-					location.reload();
+					var req = $.ajax( {url: "http://secrets.ci.northwestern.edu:3000",
+						type: "GET",
+						data: {name: username, 
+					       email: myemail,
+					       type:"denied" }
+				 	})
+		     		.done(function() {
+						window.location.href = "reviewSecrets.html?submit=true"
+			 		})
 				}
 			})
 		},
