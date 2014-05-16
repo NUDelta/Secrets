670a55ed1029bd348b33ec8a86156362d69d527c
cegrief <cegrief@gmail.com>
2014-05-16 12:05:51 -0500

    signup updated

--- includes/login.js
+++ includes/login.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 	Parse.initialize("fp7oxuptKJ9ysesuXOeV4Ieul8ErSZklVwRslkJW", "HLpukqho21z1LaL7dUrPMRWI0jAu38NqmmL9qIfo");
+	$('#spasswordconfirm, #spassword').keyup(function(){
+		if ($("#spassword").val()==$("#spasswordconfirm").val()){
+			$("#spass, #spassc").removeClass("has-error")
+			$("#spass, #spassc").addClass("has-success")
+		}
+		else{
+			$("#spass, #spassc").addClass("has-error")
+		}
+	})
 });
 
 function login(){
@@ -7,7 +16,29 @@ function login(){
 	password = $('#password').val()
 	Parse.User.logIn(username, password, {
   		success: function(user) {
-  			window.location.href = "secretsList.html"
+  			window.location.href = "secretsList.html?login=true"
   		}
   	});
-  }
+}
+
+function signup(){
+	if($("#spassword").val()!=$("#spasswordconfirm").val()){
+		$("#badpass").show()
+		return;
+	}
+	var user = new Parse.User()
+	user.set("username",$("#suser").val())
+	user.set("password",$("#spassword").val())
+	user.set("email",$("#semail").val())
+	user.signUp(null,{
+		success:function(user){
+			console.log("asdf")
+			window.location.href = "secretsList.html?login=true"
+		},
+		error:function(user, error){
+			$('#errormessage').text(error.code + ": " + error.message)
+			$('#badpass').show()
+		}
+	})
+
+}
